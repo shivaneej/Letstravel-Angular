a61b807b407133526b5fc9efa36f3abaf92a80c9
Shivanee <Shivaneej02@gmail.com>
2018-11-01 18:52:26 +0530

    1

---
+++ src/app/tab.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tab',
+  templateUrl: './tab.html',
+  styleUrls: ['./tab.css']
+})
+export class AppComponent {
+  title = 'LT';
+}
